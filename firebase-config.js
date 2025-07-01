b82aef32c93ed0d0b5a661d697d56213a1e24511
Jefferson Molina <molinajefferson001@gmail.com>
2025-07-01 15:21:02 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -2,7 +2,7 @@ const firebaseConfig = {
       apiKey: "AIzaSyD9LCzMuEGIiRQrOVCgH6d-jxxJLZ0QBCk",
       authDomain: "order-monitoring-purchasing.firebaseapp.com",
       projectId: "order-monitoring-purchasing",
-      storageBucket: "order-monitoring-purchasing.firebasestorage.app",
+      storageBucket: "order-monitoring-purchasing.appspot.com",
       messagingSenderId: "209378598320",
       appId: "1:209378598320:web:0c143d69bc329a994d8a75",
       measurementId: "G-PPNWT4ECV0"
